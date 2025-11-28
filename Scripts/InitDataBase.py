92f3416104acbbec874561254e4e89cbc670fb1e
Andrey Horoshilov <horoshcilov14@gmail.com>
2025-11-28 13:21:19 +0700

    Мелкие правки

--- Scripts/InitDataBase.py
+++ Scripts/InitDataBase.py
@@ -35,7 +35,28 @@ def create_database():
             Time             TEXT
         )  
         ''')
-        
+        # Создаем таблицу пользователей
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users (
+        Id       INTEGER PRIMARY KEY AUTOINCREMENT,
+        Login    TEXT UNIQUE NOT NULL,
+        Password TEXT NOT NULL,
+        Token    TEXT UNIQUE NOT NULL
+        )
+        ''')
+
+        # Создаем таблицу контроллеров
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Controllers (
+        Id          INTEGER PRIMARY KEY AUTOINCREMENT,
+        ChipId      TEXT NOT NULL,
+        DeviceName  TEXT,
+        UserId      INTEGER NOT NULL,
+        FOREIGN KEY (UserId) REFERENCES Users (Id),
+        UNIQUE(ChipId)
+        )
+        ''')
+
         # Сохраняем изменения
         conn.commit()
         print("База данных успешно создана!")
